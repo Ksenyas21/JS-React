379f7e61445d1be12d5d6a359214b8ef291dc283
Ksenia <56151288+Ksenyas21@users.noreply.github.com>
2020-09-21 20:13:24 +0300

    First commit

---
+++ JS/basedScript.js
@@ -0,0 +1,13 @@
+//
+// const b = confirm("Are you here?");
+// console.log(b);
+// const answer = prompt("How old are you?");
+// console.log(answer);
+// const answers=[];
+// answers[0] = prompt("Name");
+// answers[1] = prompt("LastName");
+// answers[2] = prompt("ThirdName");
+// document.write(answers)
+//---------------------------------------
+let Name = prompt("Как тебя зовут");
+document.write(`Привет ${Name} !`);
